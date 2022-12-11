048526c03b23916d59edeadfafef55205a500568
Pavel Reshetnikov <pasha.keyzet@yandex.ru>
2022-12-12 01:43:25 +0500

    whois

--- queryfactory.py
+++ queryfactory.py
@@ -1,6 +1,10 @@
 import abc
+import logging
+from typing import Literal
+
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.layers.inet import TCP, UDP, ICMP, IP
-from scapy.layers.inet6 import IPv6
+from scapy.layers.inet6 import IPv6, ICMPv6EchoRequest
 
 
 class QueryFactory(abc.ABC):
@@ -31,6 +35,11 @@ class IcmpQueryFactory(QueryFactory):
         return ICMP()
 
 
+class Icmpv6QueryFactory(QueryFactory):
+    def get_query(self):
+        return ICMPv6EchoRequest()
+
+
 class IpQueryFactory(abc.ABC):
     @abc.abstractmethod
     def get_query(self, ttl: int):
@@ -38,18 +47,36 @@ class IpQueryFactory(abc.ABC):
 
 
 class Ipv4QueryFactory(IpQueryFactory):
-    def __init__(self, ip, factory: QueryFactory):
+    def __init__(self, ip, protocol: Literal['tcp', 'udp', 'icmp'], port: int = None):
         self.__ip = ip
-        self.__factory = factory
+        self.__factory = self.__get_query_factory(protocol, port)
 
     def get_query(self, ttl: int):
         return IP(dst=self.__ip, ttl=ttl) / self.__factory.get_query()
 
+    @staticmethod
+    def __get_query_factory(protocol: Literal['tcp', 'udp', 'icmp'], port: int):
+        if protocol == 'tcp':
+            return TcpQueryFactory(port)
+        elif protocol == 'udp':
+            return UdpQueryFactory(port)
+        elif protocol == 'icmp':
+            return IcmpQueryFactory()
+
 
 class Ipv6QueryFactory(IpQueryFactory):
-    def __init__(self, ip, factory: QueryFactory):
+    def __init__(self, ip, protocol: Literal['tcp', 'udp', 'icmp'], port: int = None):
         self.__ip = ip
-        self.__factory = factory
+        self.__factory = self.__get_query_factory(protocol, port)
 
     def get_query(self, ttl: int):
         return IPv6(dst=self.__ip, hlim=ttl) / self.__factory.get_query()
+
+    @staticmethod
+    def __get_query_factory(protocol: Literal['tcp', 'udp', 'icmp'], port: int):
+        if protocol == 'tcp':
+            return TcpQueryFactory(port)
+        elif protocol == 'udp':
+            return UdpQueryFactory(port)
+        elif protocol == 'icmp':
+            return Icmpv6QueryFactory()
--- traceroute.py
+++ traceroute.py
@@ -3,29 +3,34 @@ from ipaddress import ip_address, IPv4Address, IPv6Address
 
 import scapy.sendrecv
 import contextlib
+import ipwhois
+from scapy.packet import Packet
 
 import parser
 import queryfactory
 
-
 class Traceroute:
-    def __init__(self, ip, query_factory: queryfactory.IpQueryFactory, max_queries_count, timeout):
+    def __init__(self, ip, query_factory: queryfactory.IpQueryFactory, max_queries_count, timeout, is_verbose):
         self.__ip = ip
         self.__query_factory = query_factory
         self.__max_queries_count = max_queries_count
         self.__timeout = timeout
+        self.__is_verbose = is_verbose
 
     def traceroute(self):
-        for i, response in enumerate(self.get_responses()):
-            ip = response.src if response else '*'
-            yield i + 1, ip
+        for i, (response, time) in enumerate(self.get_responses()):
+            ip = response.src if response else None
+            result = [i + 1, ip, time]
+            if self.__is_verbose:
+                result.append(_whois(ip))
+            yield _convert_none_to_star(result)
 
     def get_responses(self):
         time_to_live = 0
         while self.__have_to_send_queries(time_to_live):
             time_to_live += 1
-            response = self.__send_query(time_to_live)
-            yield response
+            response, time = self.__send_query(time_to_live)
+            yield response, time
             if self.__is_destination_response(response):
                 return
 
@@ -39,23 +44,34 @@ class Traceroute:
 
     def __send_query(self, ttl):
         query = self.__query_factory.get_query(ttl)
-        return scapy.sendrecv.sr1(query, timeout=self.__timeout, verbose=0)
+        response = scapy.sendrecv.sr1(query, timeout=self.__timeout, verbose=0)
+        time = self.__get_time(query, response)
+        return response, time
 
+    def __get_time(self, sent_packet: Packet, received_packet: Packet):
+        if received_packet:
+            return _seconds_to_milliseconds(received_packet.time - sent_packet.sent_time)
+        return _seconds_to_milliseconds(self.__timeout)
 
-def __get_query_factory(protocol: Literal['tcp', 'udp', 'icmp'], port: int):
-    if protocol == 'tcp':
-        return queryfactory.TcpQueryFactory(port)
-    elif protocol == 'udp':
-        return queryfactory.UdpQueryFactory(port)
-    elif protocol == 'icmp':
-        return queryfactory.IcmpQueryFactory()
+def _convert_none_to_star(elements: list):
+    for element in elements:
+        yield element if element else '*'
 
+def _seconds_to_milliseconds(time_in_seconds):
+    return round(time_in_seconds * 1000)
 
-def __get_ip_query_factory(ip: str, factory: queryfactory.QueryFactory):
+def _whois(ip: str):
+    with contextlib.suppress(ipwhois.BaseIpwhoisException, ValueError):
+        return _get_autonomous_system_number(ipwhois.IPWhois(ip))
+
+def _get_autonomous_system_number(ip_whois: ipwhois.IPWhois):
+    return ip_whois.lookup_whois()['asn']
+
+def __get_ip_query_factory(ip: str, protocol: Literal['tcp', 'udp', 'icmp'], port: int = None):
     if __is_ipv4(ip):
-        return queryfactory.Ipv4QueryFactory(ip, factory)
+        return queryfactory.Ipv4QueryFactory(ip, protocol, port)
     elif __is_ipv6(ip):
-        return queryfactory.Ipv6QueryFactory(ip, factory)
+        return queryfactory.Ipv6QueryFactory(ip, protocol, port)
 
 
 def __is_ipv4(ip: str):
@@ -70,8 +86,7 @@ def __is_ipv6(ip: str):
 
 if __name__ == '__main__':
     arguments = parser.parse()
-    factory = __get_query_factory(arguments.protocol, arguments.p)
-    ip_factory = __get_ip_query_factory(arguments.ip, factory)
-    traceroute = Traceroute(arguments.ip, ip_factory, arguments.n, arguments.t)
+    ip_factory = __get_ip_query_factory(arguments.ip, arguments.protocol, arguments.p)
+    traceroute = Traceroute(arguments.ip, ip_factory, arguments.n, arguments.t, arguments.v)
     for answer in traceroute.traceroute():
         print(*answer)
